cc2521ca2b6a2a40e7fc7abac968b0a6afb95925
Oreabetse Makgatlo <ogodisengo@gmail.com>
2026-08-01 18:39:28 +0200

    initial

---
+++ scripts/enemy.gd
@@ -0,0 +1,68 @@
+extends CharacterBody2D
+
+var health = 100
+var player_inattack_zone=false
+var speed=70
+var player_chase=false
+var player=null
+var can_take_damage =true
+
+
+
+
+ 
+
+
+
+func  _physics_process(delta: float):
+	deal_with_damage()
+	
+	if player_chase:
+		position +=(player.position-position)/speed
+		$AnimatedSprite2D.play("walk")
+		if (player.position.x -position.x)<0:
+			$AnimatedSprite2D.flip_h=true
+		else:
+			$AnimatedSprite2D.flip_h=false
+	else:
+		$AnimatedSprite2D.play("idle")
+
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player=body
+	player_chase=true
+	
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player=null
+	player_chase=false
+	
+func enemy():
+	pass
+
+
+func _on_enemyhitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone=true
+
+
+func _on_enemyhitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone=false
+		
+func deal_with_damage():
+	if player_inattack_zone and global.player_current_attack==true:
+		if can_take_damage==true:
+			health= health-20
+			$take_damage_cooldown.start(4.0)
+			can_take_damage=false
+			print(health)
+			
+			if health <=0:
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage=true
